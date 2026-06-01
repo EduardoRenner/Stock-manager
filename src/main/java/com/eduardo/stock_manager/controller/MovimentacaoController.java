a259ec15914363e089dfa06d23e0dde02c027887
Eduardo <eduardorenner062@gmail.com>
2026-06-01 12:29:52 -0300

    feat: adiciona @CrossOrigin no MovimentacaoController

--- src/main/java/com/eduardo/stock_manager/controller/MovimentacaoController.java
+++ src/main/java/com/eduardo/stock_manager/controller/MovimentacaoController.java
@@ -3,6 +3,7 @@ package com.eduardo.stock_manager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.eduardo.stock_manager.model.Movimentacao;
 import com.eduardo.stock_manager.service.MovimentacaoService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/movimentacoes")
 public class MovimentacaoController {
     @Autowired
