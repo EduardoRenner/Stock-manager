e42dfd73b1ed454c9de3c0652f3a4a570c8cadd6
Eduardo <eduardorenner062@gmail.com>
2026-05-31 23:34:56 -0300

    feat: adiciona @CrossOrigin no ItemController

--- src/main/java/com/eduardo/stock_manager/controller/ItemController.java
+++ src/main/java/com/eduardo/stock_manager/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.eduardo.stock_manager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.eduardo.stock_manager.service.ItemService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/itens")
 public class ItemController {
     @Autowired
